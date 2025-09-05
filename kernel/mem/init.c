15b34be8ab15c31275969372c161ac47778cd40c
harimishal1 <harimishal1@gmail.com>
2025-09-05 15:03:25 +0200

    definitely aligned1

--- kernel/mem/init.c
+++ kernel/mem/init.c
@@ -155,12 +155,18 @@ void page_init(struct boot_info *boot_info)
             if (pa >= BOOT_MAP_LIM) {
                 break;	
             }
-			if (pa == 0){
+			if (pa == 0 ||
+				(pa >= ROUNDDOWN(KERNEL_LMA, PAGE_SIZE) && pa < end) ||
+				((pa >= ROUNDDOWN(PADDR(boot_info), PAGE_SIZE) && 
+				pa < ROUNDUP(PADDR(boot_info) + sizeof(*boot_info), PAGE_SIZE)) ||
+				(pa >= ROUNDDOWN((physaddr_t)(boot_info->elf_hdr), PAGE_SIZE)
+				&& pa < ROUNDUP((physaddr_t)(boot_info->elf_hdr + sizeof(*boot_info->elf_hdr)),PAGE_SIZE)))){
+					
 				page = pa2page(pa);
 				page->pp_avail = 1;
 				continue;
 			}
-			if (pa >= ROUNDDOWN(KERNEL_LMA, PAGE_SIZE) && pa < end) {
+			/* if (pa >= ROUNDDOWN(KERNEL_LMA, PAGE_SIZE) && pa < end) {
 				page = pa2page(pa);
 				page->pp_avail = 1;
 				continue;
@@ -171,13 +177,12 @@ void page_init(struct boot_info *boot_info)
 				page->pp_avail = 1;
                 continue;
             }
-			//ELF HEADER ?????????? -> when the below block is commented out, everything works fine
 			if (pa >= ROUNDDOWN((physaddr_t)(boot_info->elf_hdr), PAGE_SIZE)
 				&& pa < ROUNDUP((physaddr_t)(boot_info->elf_hdr + sizeof(*boot_info->elf_hdr)),PAGE_SIZE)){
 				page = pa2page(pa);
 				page->pp_avail = 1;
 				continue;
-			} 
+			}  */
             page = pa2page(pa);
             page->pp_avail = 1;
 			if (page->pp_ref == 0) {
