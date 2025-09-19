2381b727750f34b8c70bd512ff8f8e7113a58a61
eva <eva@imbens.nl>
2025-09-19 15:19:32 +0200

    Start on syscall bonus

--- kernel/sched/syscall.c
+++ kernel/sched/syscall.c
@@ -20,6 +20,12 @@ extern void syscall64(void);
 void syscall_init(void)
 {
 	/* LAB 3: your bonus code here. */
+#ifdef BONUS_SYSCALL
+	write_msr(MSR_STAR, ((uint64_t)GDT_KCODE << 32) | ((uint64_t)GDT_UCODE << 48));
+	write_msr(MSR_LSTAR, (uint64_t)syscall64);
+	write_msr(MSR_SFMASK, IF_RFLAGS);
+	write_msr(MSR_EFER, read_msr(MSR_EFER) | MSR_EFER_SCE);
+	#endif
 }
 
 
