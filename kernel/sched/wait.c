e10fa764b3f5354054d830d0adf1c13e5b8d41f2
eva <eva@imbens.nl>
2025-10-03 12:34:32 +0200

    Change sys_wait and sys_waitpid

--- kernel/sched/wait.c
+++ kernel/sched/wait.c
@@ -15,9 +15,12 @@ pid_t sys_wait(int *rstatus)
     struct list *node;
     struct task *child;
 
-   if (list_is_empty(&cur_task->task_children) && list_is_empty(&cur_task->task_zombies)) {
+    if (list_is_empty(&cur_task->task_children) && list_is_empty(&cur_task->task_zombies)) {
         return -ECHILD; 
     }
+
+    cur_task->task_wait = NULL;
+    cur_task->task_status = TASK_NOT_RUNNABLE;
 
     list_foreach(&cur_task->task_zombies, node) {
         child = container_of(node, struct task, task_node);
@@ -25,13 +28,15 @@ pid_t sys_wait(int *rstatus)
         if (rstatus) {
             *rstatus = child->task_exit_status;
         }
+        pid_t return_pid = child->task_pid;
+        list_del(&child->task_node);
         list_del(&child->task_child);
-        task_destroy(child);
-        return child->task_pid;
+        task_free(child);
+        return return_pid;
     }
 
-    cur_task->task_wait = NULL;
-    cur_task->task_status = TASK_NOT_RUNNABLE;
+    // cur_task->task_wait = NULL;
+    // cur_task->task_status = TASK_NOT_RUNNABLE;
     cur_task->task_wait_exit_status = rstatus;
     sched_yield();
     return sys_wait(rstatus);
@@ -51,29 +56,40 @@ pid_t sys_waitpid(pid_t pid, int *rstatus, int opts)
         return sys_wait(rstatus);
     }
 
+    child = pid2task(pid, 1);
+    if (!child) {
+        return -ECHILD;
+    }
+    if (child == cur_task) {
+        return -ECHILD;
+    }
+    cur_task->task_wait = child;
+    cur_task->task_status = TASK_NOT_RUNNABLE;
+
     list_foreach(&cur_task->task_zombies, node) {
         child = container_of(node, struct task, task_node);
         if (child->task_pid == pid) {
             if (rstatus) {
                 *rstatus = child->task_exit_status;
             }
+            list_del(&child->task_node);
             list_del(&child->task_child);
-            task_destroy(child);
+            task_free(child);
             return pid;
         }
     }
 
-    child = pid2task(pid, 1);
-   /*  if (!child) {
-        return -ECHILD;
-    } */
+//     child = pid2task(pid, 1);
+//    /*  if (!child) {
+//         return -ECHILD;
+//     } */
 
-    if (child == cur_task) {
-        return -ECHILD;
-    }
+//     if (child == cur_task) {
+//         return -ECHILD;
+//     }
 
-    cur_task->task_wait = child;
-    cur_task->task_status = TASK_NOT_RUNNABLE;
+//     cur_task->task_wait = child;
+//     cur_task->task_status = TASK_NOT_RUNNABLE;
     cur_task->task_wait_exit_status = rstatus;
     cprintf("[PID %5u] Reaping task with PID %d\n", cur_task->task_pid, child->task_pid);
     sched_yield();
