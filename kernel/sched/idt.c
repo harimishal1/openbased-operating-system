6b3984bb0db4b196c0ca407dee547ef3052cb80d
harimishal1 <harimishal1@gmail.com>
2025-10-06 21:23:14 +0000

    implemented big kernel lock

--- kernel/sched/idt.c
+++ kernel/sched/idt.c
@@ -14,9 +14,16 @@
 #include <kernel/sched/syscall.h>
 
 #include <kernel/sched/task.h>
+#include <spinlock.h>
+#include <x86-64/paging.h>
 
 #include <lib.h>
 #include <paging.h>
+
+#ifdef USE_BIG_KERNEL_LOCK
+	extern struct spinlock kernel_lock;
+#endif
+
 extern int task_page_fault_handler(struct task *task, void *va, int flags);
 
 /* LAB 3: your code here. */
@@ -211,7 +218,7 @@ void irq_handler(struct int_frame *frame)
         reschedule = true;
     }
     lapic_eoi();
-	sched_yield();
+	//sched_yield();
 }
 
 void int_dispatch(struct int_frame *frame)
@@ -274,6 +281,16 @@ void int_handler(struct int_frame *frame)
 	 * "cli" in the interrupt path.
 	 */
 	assert(!(read_rflags() & FLAGS_IF));
+#ifdef USE_BIG_KERNEL_LOCK
+	bool from_user = ((frame->cs & 3) == 3);
+	if (from_user) {
+		//lock_kernel();
+		big_spin_lock(&kernel_lock);
+	}
+#else
+    bool from_user = ((frame->cs & 3) == 3);
+#endif
+
 	/* cprintf("Incoming INT frame at %p\n", frame); */
 	if ((frame->cs & 3) == 3) {
 		/* Interrupt from user mode. */
@@ -292,11 +309,16 @@ void int_handler(struct int_frame *frame)
 	int_dispatch(frame);
 
 	// LAB 5
-	if (reschedule) {
+	if (reschedule && from_user) {
     	reschedule = false;
     	sched_yield();
     }
 
+#ifdef USE_BIG_KERNEL_LOCK
+	if (cur_task && ((cur_task->task_frame.cs & 3) == 3)) {
+		big_spin_unlock(&kernel_lock);
+	}
+#endif
 	/* Return to the current task, which should be running. */
 	task_run(cur_task);
 }
